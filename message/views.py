eb51bf83644a2793fb3dacb729b5a270fa7180d4
odiegofarias <diego.farias.fernandes@gmail.com>
2022-09-25 01:15:16 -0300

    Criadas views para exibir todos os perfis e exibir perfis de usuários únicos e seus seguidos/seguidores com seus recpectivos templates

--- message/views.py
+++ message/views.py
@@ -1,5 +1,26 @@
 from django.shortcuts import render
+from .models import Profile
 
 
 def dashboard(request):
     return render(request, "base.html")
+
+def profile_list(request):
+    profiles = Profile.objects.exclude(usuario=request.user)
+    return render(
+        request,
+        "message/profile_list.html",
+        {
+            "profiles": profiles,
+        }
+    )
+
+def profile(request, pk):
+    profile = Profile.objects.get(pk=pk)
+    return render(
+        request,
+        "message/profile.html",
+        {
+            "profile": profile,
+        }
+    )
